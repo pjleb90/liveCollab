606b24aa721a0361fa9139aca3ed394634ed94dd
pjleb90 <pjleb90@gmail.com>
2024-08-05 11:13:47 -0400

    added Header component and type definitions

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Header = ({children}: HeaderProps) => {
+  return (
+    <div className='header'>
+        <Link href='/' className='md:flex-1'>
+            <Image src="/assets/icons/logo.svg" width={120} height={32} alt="logo with name" className='hidden md:block'/>
+            <Image src="/assets/icons/logo-icon.svg" width={32} height={32} alt="logo" className='mr-2 md:hidden' />
+        </Link>
+        {children}
+    </div>
+  )
+}
+
+export default Header
