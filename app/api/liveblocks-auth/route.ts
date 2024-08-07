3993b477c39059d2190bcbb9efa5fb69edbc07bf
pjleb90 <pjleb90@gmail.com>
2024-08-07 14:14:44 -0400

    collaborative editor functionality

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -1,13 +1,15 @@
-import { liveblocks } from "@/lib/liveblock";
+import { liveblocks } from "@/lib/liveblocks";
 import { getUserColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export async function POST(request: Request) {
     const clerkUser = await currentUser();
+
     if(!clerkUser) redirect('/sign-in');
 
     const { id, firstName, lastName, emailAddresses, imageUrl } = clerkUser;
+
   // Get the current user from your database
     const user = {
         id,
@@ -23,8 +25,8 @@ export async function POST(request: Request) {
     // Identify the user and return the result
     const { status, body } = await liveblocks.identifyUser(
         {
-            userId: user.info.email,
-            groupIds: [], // Optional
+        userId: user.info.email,
+        groupIds: [],
         },
         { userInfo: user.info },
     );
--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -4,33 +4,37 @@ import React from 'react'
 import {
     RoomProvider,
     ClientSideSuspense,
-  } from "@liveblocks/react/suspense";
-  import {
+} from "@liveblocks/react/suspense";
+import {
     SignInButton,
     SignedIn,
     SignedOut,
     UserButton
-  } from '@clerk/nextjs'
-  import Header from '@/components/Header'
-  import { Editor } from '@/components/editor/Editor'
+} from '@clerk/nextjs'
+import Header from '@/components/Header'
+import { Editor } from '@/components/editor/Editor'
+import ActiveCollaborators from './ActiveCollaborators';
 
-const CollaborativeRoom = () => {
-  return (
-        <RoomProvider id="my-room">
+const CollaborativeRoom = ( { roomId, roomMetadata } : CollaborativeRoomProps ) => {
+    return (
+        <RoomProvider id={roomId}>
             <ClientSideSuspense fallback={<div>Loading…</div>}>
                 <div className='collaborative-room'>
                     <Header>
                         <div className='flex w-fit items-center justify-center gap-2'>
-                        <p className='document-title'>
-                            Share
-                        </p>
+                            <p className='document-title'>
+                                Share
+                            </p>
                         </div>
-                        <SignedOut>
-                            <SignInButton />
-                        </SignedOut>
-                        <SignedIn>
-                            <UserButton />
-                        </SignedIn>
+                        <div className='flex w-full flex-1 justify-end gap-2 sm:gap-3'>
+                            <ActiveCollaborators />
+                            <SignedOut>
+                                <SignInButton />
+                            </SignedOut>
+                            <SignedIn>
+                                <UserButton />
+                            </SignedIn>
+                        </div>
                     </Header>
                     <Editor />
                 </div>
