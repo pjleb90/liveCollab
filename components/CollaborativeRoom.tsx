6dd5acc590bc629f3554754f49bb08af0674d60c
pjleb90 <pjleb90@gmail.com>
2024-08-08 14:12:43 -0400

    added floating comment functionality

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -20,12 +20,10 @@ import ActiveCollaborators from './ActiveCollaborators';
 import { Input } from './ui/input';
 import { updateDocument } from '@/lib/actions/room.actions';
 
-const CollaborativeRoom = ( { roomId, roomMetadata } : CollaborativeRoomProps ) => {
+const CollaborativeRoom = ( { roomId, roomMetadata, users, currentUserType } : CollaborativeRoomProps ) => {
     const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
     const [editing, setEditing] = useState(false);
     const [loading, setLoading] = useState(false);
-
-    const currentUserType = 'editor';
 
     const containerRef = useRef<HTMLDivElement>(null);
     const inputRef = useRef<HTMLInputElement>(null);
@@ -118,7 +116,7 @@ const CollaborativeRoom = ( { roomId, roomMetadata } : CollaborativeRoomProps ) 
                             </SignedIn>
                         </div>
                     </Header>
-                    <Editor />
+                    <Editor roomId={roomId} currentUserType={currentUserType}/>
                 </div>
             </ClientSideSuspense>
         </RoomProvider>
