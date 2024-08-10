c52436c9df021ab0725e8c09749ca8e145df2491
pjleb90 <pjleb90@gmail.com>
2024-08-10 13:28:22 -0400

    formatting

--- components/loader.tsx
+++ components/loader.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Loader = () => {
   return (
     <div className="loader">
-      <Image 
+      <Image
         src="/assets/icons/loader.svg"
         alt="loader"
         width={32}
